7778cf160d662fd01c8fce15800b75f90311331b
Sebastian Berchtold <Seb.Berchtold@web.de>
2014-11-28 01:28:23 +0100

    Inital commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='odm-catalogs',
+    description="Extraction of metadata from various data catalogs",
+    license='mit',
+    packages=find_packages
+)
