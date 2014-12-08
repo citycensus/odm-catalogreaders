d0f79055032bc4087de3dcdc68aafc37e475b15b
Sebastian Berchtold <Seb.Berchtold@web.de>
2014-12-08 19:51:51 +0100

    Update package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='odm-catalogs',
+    name='odm-catalogreaders',
     description="Extraction of metadata from various data catalogs",
     license='mit',
     packages=['odm', 'odm.catalogs', 'odm.catalogs.portals', 'odm.catalogs.utils'],
